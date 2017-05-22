961d61ef804d9cc8f2408f7de62eee3165203e58
Chris Wright <daverandom@php.net>
2017-05-22 16:47:57 +0100

    Add and use public normalize_name() function

--- src/Records/Types/DomainName.php
+++ src/Records/Types/DomainName.php
@@ -25,11 +25,6 @@ class DomainName extends Type
     const FLAG_NO_COMPRESSION = 0x80000000;
 
     /**
-     * @var callable|null
-     */
-    private static $labelPreProcessor = null;
-
-    /**
      * @var string
      */
     protected $value = '';
@@ -47,27 +42,6 @@ class DomainName extends Type
      */
     public function __construct($value = null)
     {
-        if (!isset(self::$labelPreProcessor)) {
-            self::$labelPreProcessor = \function_exists('idn_to_ascii')
-                ? static function($label) {
-                      if (false === $result = \idn_to_ascii($label, 0, INTL_IDNA_VARIANT_UTS46)) {
-                          throw new \InvalidArgumentException("Label '{$label}' could not be processed for IDN");
-                      }
-
-                      return $result;
-                  }
-                : static function($label) {
-                      if (\preg_match('/[\x80-\xff]/', $label)) {
-                          throw new \RuntimeException(
-                              "Label '{$label}' contains non-ASCII characters and IDN support is not available."
-                              . " Verify that ext/intl is installed for IDN support."
-                          );
-                      }
-
-                      return \strtolower($label);
-                  };
-        }
-
         if (\is_array($value)) {
             $this->setLabels($value);
         } else {
@@ -113,7 +87,7 @@ class DomainName extends Type
         $length = $count = 0;
 
         foreach ($labels as &$label) {
-            $label = (self::$labelPreProcessor)($label);
+            $label = \LibDNS\normalize_name($label);
             $labelLength = \strlen($label);
             if ($labelLength > 63) {
                 throw new \InvalidArgumentException('Label list is not a valid domain name: Label ' . $label . ' length exceeds 63 byte limit');
