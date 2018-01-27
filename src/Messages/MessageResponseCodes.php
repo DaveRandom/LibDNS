8bdfe280a36b2a2eb7588a87c2bf3c88c7f0072e
Chris Wright <daverandom@php.net>
2018-01-27 18:39:59 +0000

    Add missing message response codes

--- src/Messages/MessageResponseCodes.php
+++ src/Messages/MessageResponseCodes.php
@@ -12,4 +12,18 @@ final class MessageResponseCodes extends Enum
     const NAME_ERROR = 3;
     const NOT_IMPLEMENTED = 4;
     const REFUSED = 5;
+    const UNEXPECTED_EXTANT_DOMAIN = 6;
+    const UNEXPECTED_EXTANT_RRSET = 7;
+    const NON_EXISTENT_RRSET = 8;
+    const SERVER_NOT_AUTHORITATIVE = 9;
+    const NOT_AUTHORIZED = 9;
+    const BAD_OPT_VERSION = 16;
+    const BAD_SIGNATURE = 16;
+    const BAD_KEY = 17;
+    const BAD_TIME = 18;
+    const BAD_MODE = 19;
+    const BAD_NAME = 20;
+    const BAD_ALGORITHM = 21;
+    const BAD_TRUNCATION = 22;
+    const BAD_COOKIE = 23;
 }
