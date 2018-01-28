f1d8712c976d9ad252bc77ca5c8b3a76e5846947
Chris Wright <daverandom@php.net>
2018-01-28 22:22:17 +0000

    Normalise and validate names for hosts file lookup

--- src/HostsFile/HostsFile.php
+++ src/HostsFile/HostsFile.php
@@ -2,6 +2,7 @@
 
 namespace DaveRandom\LibDNS\HostsFile;
 
+use DaveRandom\Network\DomainName;
 use DaveRandom\Network\IPAddress;
 
 final class HostsFile
@@ -13,18 +14,37 @@ final class HostsFile
         $this->map = $map;
     }
 
+    /**
+     * @param string|DomainName $name
+     */
     public function containsName($name, int $family = \STREAM_PF_INET): bool
     {
+        if (!$name instanceof DomainName) {
+            try {
+                $name = DomainName::createFromString((string)$name);
+            } catch (\InvalidArgumentException $e) {
+                return false;
+            }
+        }
+
         return isset($this->map[(string)$name][$family]);
     }
 
     public function getAddressForName($name, int $family = \STREAM_PF_INET): IPAddress
     {
-        if (!isset($this->map[$name = (string)$name][$family])) {
+        if (!$name instanceof DomainName) {
+            try {
+                $name = (string)DomainName::createFromString((string)$name);
+            } catch (\InvalidArgumentException $e) {
+                $name = null;
+            }
+        }
+
+        if (!isset($name, $this->map[(string)$name][$family])) {
             throw new \OutOfBoundsException("No record defined for name {$name} in address family {$family}");
         }
 
-        return $this->map[$name][$family];
+        return $this->map[(string)$name][$family];
     }
 
     public function toArray(): array
